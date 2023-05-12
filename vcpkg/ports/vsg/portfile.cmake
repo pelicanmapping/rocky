1453d7e31d879777a9e3479bf814ed8c034f8584
Paul Levy <paul.levy@gmail.com>
2023-05-12 13:24:32 -0400

    move up to 1.0.6 master
    Keep vcpkg vsg portfile but more recent commit so not stuck at 1.0.5

--- vcpkg/ports/vsg/portfile.cmake
+++ vcpkg/ports/vsg/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vsg-dev/VulkanSceneGraph
-    REF "v${VERSION}"
-    SHA512 da5c9448da6bd2bd6edcb82ed1ac67aded9c3fd957fb7d3c78bb741d8993a5e90b134433237a63bf1a6eb23b30a1e6b4ea2115416f8d32464bd04925d8dbcd34
+    REF e775af7f08a1890ef7b5ba402474dce687790835
+    SHA512 fcdabc1bca3bdf663a125d70efe5d6f1cf57304af325d4262e9343cb30246ae35ad88b4acd3fc3ae6bc9b60326c68be6fd6b138bb6e8a9e17aaf890495b4c5cc
     HEAD_REF master
 	PATCHES devendor-glslang.patch
 )
