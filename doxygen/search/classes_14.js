932f74d251c19592369d5f65e5d2bf2d289f18d4
gwaldron <gwaldron@gmail.com>
2025-03-01 15:06:24 +0000

    Deploying to gh-pages from @ pelicanmapping/rocky@13216515c77666d55a58e75902de204c6eb11683 🚀

--- doxygen/search/classes_14.js
+++ doxygen/search/classes_14.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['widgetsystemnode_0',['WidgetSystemNode',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_widget_system_node.html',1,'ROCKY_NAMESPACE']]]
+  ['widget_0',['Widget',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_widget.html',1,'ROCKY_NAMESPACE']]],
+  ['widgetsystemnode_1',['WidgetSystemNode',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_widget_system_node.html',1,'ROCKY_NAMESPACE']]]
 ];
--- doxygen/search/variables_10.js
+++ doxygen/search/variables_10.js
@@ -4,10 +4,11 @@ var searchData=
   ['readerwriteroptions_1',['readerWriterOptions',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_v_s_g_context_impl.html#ab11706b2e6e78ea5e7a3683064e09046',1,'ROCKY_NAMESPACE::VSGContextImpl']]],
   ['referencepoint_2',['referencepoint',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_line.html#ae3acd64289ba361832af68520bd6f6a1',1,'ROCKY_NAMESPACE::Line::referencePoint'],['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_mesh.html#a6869bcd2499410b4e44299404b599734',1,'ROCKY_NAMESPACE::Mesh::referencePoint']]],
   ['referrer_3',['referrer',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_i_o_options.html#a5ad5b558fff42ea58e9c16f88114283a',1,'ROCKY_NAMESPACE::IOOptions']]],
-  ['rendercontinuously_4',['renderContinuously',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_v_s_g_context_impl.html#a7841f63786f169eddf8c241edb8135cd',1,'ROCKY_NAMESPACE::VSGContextImpl']]],
-  ['renderingenabled_5',['renderingEnabled',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_v_s_g_context_impl.html#a60b06e8377def6bbca78f57e6f875c12',1,'ROCKY_NAMESPACE::VSGContextImpl']]],
-  ['renderrequests_6',['renderRequests',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_v_s_g_context_impl.html#a1008c3fa307122c16190dffeb1e0c0ed',1,'ROCKY_NAMESPACE::VSGContextImpl']]],
-  ['requiresupdate_7',['requiresUpdate',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_terrain_tile_model.html#a4924feca0b53f6ee1228ff5e2c194814',1,'ROCKY_NAMESPACE::TerrainTileModel']]],
-  ['revision_8',['revision',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_terrain_tile_model.html#ab5460b936d592cf11f623c1e3305fdf2',1,'ROCKY_NAMESPACE::TerrainTileModel::revision'],['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_revisioned_component.html#af4a02a31ffe733c0feff90840b0c035c',1,'ROCKY_NAMESPACE::RevisionedComponent::revision']]],
-  ['runtime_9',['runtime',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_terrain_engine.html#ab93462ed24c29dec249b73a456f4d773',1,'ROCKY_NAMESPACE::TerrainEngine']]]
+  ['render_4',['render',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_widget.html#a25efe20b72a2222434520444e12219ba',1,'ROCKY_NAMESPACE::Widget']]],
+  ['rendercontinuously_5',['renderContinuously',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_v_s_g_context_impl.html#a7841f63786f169eddf8c241edb8135cd',1,'ROCKY_NAMESPACE::VSGContextImpl']]],
+  ['renderingenabled_6',['renderingEnabled',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_v_s_g_context_impl.html#a60b06e8377def6bbca78f57e6f875c12',1,'ROCKY_NAMESPACE::VSGContextImpl']]],
+  ['renderrequests_7',['renderRequests',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_v_s_g_context_impl.html#a1008c3fa307122c16190dffeb1e0c0ed',1,'ROCKY_NAMESPACE::VSGContextImpl']]],
+  ['requiresupdate_8',['requiresUpdate',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_terrain_tile_model.html#a4924feca0b53f6ee1228ff5e2c194814',1,'ROCKY_NAMESPACE::TerrainTileModel']]],
+  ['revision_9',['revision',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_terrain_tile_model.html#ab5460b936d592cf11f623c1e3305fdf2',1,'ROCKY_NAMESPACE::TerrainTileModel::revision'],['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_revisioned_component.html#af4a02a31ffe733c0feff90840b0c035c',1,'ROCKY_NAMESPACE::RevisionedComponent::revision']]],
+  ['runtime_10',['runtime',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_terrain_engine.html#ab93462ed24c29dec249b73a456f4d773',1,'ROCKY_NAMESPACE::TerrainEngine']]]
 ];
