e9eba12460a2d656998eba566b618ff5f8c7833f
gwaldron <gwaldron@gmail.com>
2025-03-28 19:48:54 +0000

    Deploying to gh-pages from @ pelicanmapping/rocky@3db5aea012138ee362837e464297bc4f95fcfd57 🚀

--- doxygen/search/variables_9.js
+++ doxygen/search/variables_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['image_0',['image',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_icon.html#aac1b0704fee8541fa4b641174fed78b6',1,'ROCKY_NAMESPACE::Icon']]],
-  ['invalid_1',['INVALID',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_tile_key.html#a64985cd8a5ad12099bec3b996206e289',1,'ROCKY_NAMESPACE::TileKey']]]
+  ['in_5fplace_5fdelete_1',['in_place_delete',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_transform.html#a72c97453f79944cb94b2a47936fee95d',1,'ROCKY_NAMESPACE::Transform']]],
+  ['invalid_2',['INVALID',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_tile_key.html#a64985cd8a5ad12099bec3b996206e289',1,'ROCKY_NAMESPACE::TileKey']]]
 ];
