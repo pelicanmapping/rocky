148c997109650fc02f168c72954746d89efe3460
gwaldron <gwaldron@gmail.com>
2025-07-14 19:59:38 +0000

    Deploying to gh-pages from @ pelicanmapping/rocky@3344f393362b134d4dae1640a3ba2cbd229e6b85 🚀

--- doxygen/search/classes_d.js
+++ doxygen/search/classes_d.js
@@ -14,10 +14,9 @@ var searchData=
   ['option_3c_20rocky_5fnamespace_3a_3aprofile_20_3e_11',['option&lt; ROCKY_NAMESPACE::Profile &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
   ['option_3c_20rocky_5fnamespace_3a_3auri_20_3e_12',['option&lt; ROCKY_NAMESPACE::URI &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
   ['option_3c_20std_3a_3astring_20_3e_13',['option&lt; std::string &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
-  ['option_3c_20typedoptions_20_3e_14',['option&lt; TypedOptions &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
-  ['option_3c_20unsigned_20_3e_15',['option&lt; unsigned &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
-  ['option_3c_20vsg_3a_3abuttonpressevent_20_3e_16',['option&lt; vsg::ButtonPressEvent &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
-  ['option_3c_20vsg_3a_3akeypressevent_20_3e_17',['option&lt; vsg::KeyPressEvent &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
-  ['option_3c_20vsg_3a_3amoveevent_20_3e_18',['option&lt; vsg::MoveEvent &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
-  ['option_3c_20vsg_3a_3atime_5fpoint_20_3e_19',['option&lt; vsg::time_point &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]]
+  ['option_3c_20unsigned_20_3e_14',['option&lt; unsigned &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
+  ['option_3c_20vsg_3a_3abuttonpressevent_20_3e_15',['option&lt; vsg::ButtonPressEvent &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
+  ['option_3c_20vsg_3a_3akeypressevent_20_3e_16',['option&lt; vsg::KeyPressEvent &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
+  ['option_3c_20vsg_3a_3amoveevent_20_3e_17',['option&lt; vsg::MoveEvent &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]],
+  ['option_3c_20vsg_3a_3atime_5fpoint_20_3e_18',['option&lt; vsg::time_point &gt;',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1option.html',1,'ROCKY_NAMESPACE']]]
 ];
