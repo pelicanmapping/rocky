4b6eaa377d697408d62b61784808b36ccf08b97e
Glenn Waldron <gwaldron@gmail.com>
2026-02-26 16:57:28 -0500

    Add --model viewer to rocky_simple

--- src/apps/rocky_simple/rocky_simple.cpp
+++ src/apps/rocky_simple/rocky_simple.cpp
@@ -6,9 +6,14 @@
 
 #include <rocky/rocky.h>
 
+#ifdef ROCKY_HAS_VSGXCHANGE
+#include <vsgXchange/all.h>
+#endif
+
 int simple(int argc, char** argv);
 int custom_window(int argc, char** argv);
 int no_app(int argc, char** argv);
+int model(const std::string&);
 void install_debug_layer(vsg::ref_ptr<vsg::Window>);
 bool debugLayer = false;
 
@@ -36,10 +41,15 @@ int main(int argc, char** argv)
     if (args.read("--no-app"))
         return no_app(argc, argv);
 
+    std::string filename;
+    if (args.read("--model", filename))
+        return model(filename);
+
     rocky::Log()->info("Options: ");
     rocky::Log()->info("  --simple           (rocky::Application, fully automated)");
     rocky::Log()->info("  --custom-window    (rocky::Application, but create our own window, camera, and manipulator)");
     rocky::Log()->info("  --no-app           (Manage the VSG viewer and frame loop ourselves, no rocky::Application)");
+    rocky::Log()->info("  --model            (Load and view a model)");
     return 0;
 }
 
@@ -169,6 +179,81 @@ int no_app(int argc, char** argv)
     viewer->addEventHandler(vsg::CloseHandler::create(viewer));
 
     viewer->addEventHandler(rocky::MapManipulator::create(mapNode, window, camera, vsgcontext));
+
+    // compile everything.
+    viewer->compile();
+
+    // run the VSG frame loop.
+    while (viewer->advanceToNextFrame())
+    {
+        viewer->handleEvents();
+        viewer->update();
+        viewer->recordAndSubmit();
+        viewer->present();
+    }
+
+    return 0;
+}
+
+
+int model(const std::string& filename)
+{
+    // make a viewer.
+    auto viewer = vsg::Viewer::create();
+
+#ifdef ROCKY_HAS_VSGXCHANGE
+    // Adds all the readerwriters in vsgxchange to the options data.
+    auto options = vsg::Options::create();
+    options->add(vsgXchange::all::create());
+#endif
+
+    auto model = vsg::read_cast<vsg::Node>(filename, options);
+    if (!model) {
+        std::cerr << "Failed to load model: " << filename << std::endl;
+        return -1;
+    }
+
+    vsg::ComputeBounds cb;
+    model->accept(cb);
+
+    // build a camera.
+    double nearFarRatio = 0.00001;
+    double R = vsg::length(cb.bounds.max);
+
+    auto traits = vsg::WindowTraits::create(1920, 1080, "show model");
+    auto window = vsg::Window::create(traits);
+    viewer->addWindow(window);
+
+    auto perspective = vsg::Perspective::create(
+        30.0,
+        (double)(window->extent2D().width) / (double)(window->extent2D().height),
+        R * nearFarRatio,
+        R * 10.0);
+
+    auto lookAt = vsg::LookAt::create(
+        vsg::dvec3(R * 10.0, 0.0, 0.0),
+        vsg::dvec3(0.0, 0.0, 0.0),
+        vsg::dvec3(0.0, 0.0, 1.0));
+
+    auto camera = vsg::Camera::create(
+        perspective,
+        lookAt,
+        vsg::ViewportState::create(window->extent2D()));
+
+    // build a view, render graph, and command graph.
+    auto view = vsg::View::create(camera, model);
+    auto rendergraph = vsg::RenderGraph::create(window, view);
+    auto commandgraph = vsg::CommandGraph::create(window, rendergraph);
+    viewer->assignRecordAndSubmitTaskAndPresentation({ commandgraph });
+
+    // add some event handlers.
+    viewer->addEventHandler(vsg::CloseHandler::create(viewer));
+    viewer->addEventHandler(vsg::Trackball::create(camera));
+
+    view->addChild(vsg::createHeadlight());
+    auto ambient = vsg::AmbientLight::create();
+    ambient->color = { 0.03f, 0.03f, 0.03f };
+    view->addChild(ambient);
 
     // compile everything.
     viewer->compile();
